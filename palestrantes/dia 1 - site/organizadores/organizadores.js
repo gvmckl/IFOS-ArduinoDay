a888594acc1a4221151b8925533c934e4d4b850e
Geovanna Mickaella <gvmckl@gmail.com>
2024-03-20 20:33:32 -0300

    Atualização

--- palestrantes/dia 1 - site/organizadores/organizadores.js
+++ palestrantes/dia 1 - site/organizadores/organizadores.js
@@ -4,7 +4,6 @@ buttons.forEach((button) => {
         let card = button.parentElement;
         card.classList.toggle("active");
         if (card.classList.contains("active")) {
-            // Change button text if has class active
             return (button.textContent = "Ler menos");
         }
         button.textContent = "Ler mais";
